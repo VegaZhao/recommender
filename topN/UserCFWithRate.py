cc2869b914e376e4a30091fde418bb87b279c230
VegaZhao <31203074+VegaZhao@users.noreply.github.com>
2019-04-03 16:39:19 +0800

    Update and rename UserCFWithRate to UserCFWithRate.py

--- topN/UserCFWithRate.py
+++ topN/UserCFWithRate.py
@@ -1,9 +1,11 @@
 # coding:utf-8
+# 说明：带兴趣度的用户协同过滤算法
 import numpy as np
 import pandas as pd
 import random
 import math
 import operator
+from sklearn.utils import shuffle
 
 # item相似矩阵相似度最大值归一化（一种优化方案）
 def NormalizeSimilarity(dict):
@@ -31,19 +33,32 @@ def SplitData(data, M, k, seed):
 	# 输入：
 	#	data: 二维矩阵 [item, user, rating]
 	#	M：测试集占比，训练集:测试集 = M:1
-	#	k：选取前k个相似item
+	#	k：保留随机数等于k的样本
 	#	seed：随机种子
 	# 输出：
 	#	train, test 二维列表 [item, user, rating]
     test = []
     train = []
     random.seed(seed)
-    for item, user, rate in data:
+    for user, item, rate in data:
         if random.randint(0, M) == k:
-            test.append([user,item, rate])
+            test.append([user, item, rate])
         else:
-            train.append([user,item, rate])
+            train.append([user, item, rate])
     return train, test
+
+# 将列表转成user-item字典
+def TestUserItemDict(data):
+	# 输入：
+	#	二维列表 [item, user, rating]
+	# 输出：
+	#	user_item字典 {user1 : {item1 : rate1, item2 : rate2}, ...}}
+	user_item = {}
+	for user, item, rate in data:
+		if user not in user_item:
+			user_item[user] = []
+		user_item[user].append(item)
+	return user_item
 
 # 将列表转成user-item字典
 def UserItemDict(data):
@@ -52,7 +67,7 @@ def UserItemDict(data):
 	# 输出：
 	#	user_item字典 {user1 : {item1 : rate1, item2 : rate2}, ...}}
 	user_item = {}
-	for item, user, rate in data:
+	for user, item, rate in data:
 		if user not in user_item:
 			user_item[user] = {}
 		user_item[user].update({item : rate})
@@ -65,7 +80,7 @@ def ItemUserDict(data):
 	# 输出：
 	#	item_user字典 {itemid:[user1, user2, ...]}
 	item_user = {}
-	for item, user, rate in data:
+	for user, item, rate in data:
 		if item not in item_user:
 			item_user[item] = []
 		item_user[item].append(user)
@@ -74,7 +89,7 @@ def ItemUserDict(data):
 # 计算item之间的相似度
 def UserSimilarity(item_user):
 	# 输入：
-	#	item_user字典 {itemid:[user1, user2, ...]}
+	#	item_user字典 {item1 : {user1 : rate1, user2 : rate2}, ...}}
 
 	# C[u][v]存储观看用户u和v共同看的电影数
     C = {}
@@ -117,24 +132,34 @@ def UserSimilarity(item_user):
     return W
 
 # 推荐电影
-def Recommendation(user_item, user_id, W, K):
+def Recommendation(user_item, user_id, W, K, R):
 	# 输入：
 	#	user_item: 字典 {user1 : {item1 : rate1, item2 : rate2}, ...}}
 	#	user_id：推荐的用户id
 	#	W：用户相似矩阵
-	#	K：前K个最相似用户
+	#   K：前K个最相似用户
+    #   R：推荐列表中电影个数
 	# 输出：
 	#	rank：字典，该用户的推荐电影列表
 
 	# 存储用户推荐电影
     rank = {}
+	# 开辟用户空子字典 ('rank: ', {user_id: {}})
+    rank.setdefault(user_id, {})
+
+	# 如果该用户不在训练集中，则推荐空序列
+    if user_id not in user_item:
+        print('unvalid user_id(user_id not in user_item): ', user_id)
+        return rank
+	# 如果该用户不在相似度矩阵中，则推荐空序列
+    if user_id not in W:
+        print('unvalid user_id(user_id not in W): ', user_id)
+        return rank
+
 	# 用户已观看的电影集合
     interacted_item = user_item[user_id]
 
-	# 开辟用户空子字典 ('rank: ', {user_id: {}})
-    rank.setdefault(user_id, {})
 	# 遍历相似矩阵中该用户前K个最相似用户
-
     for v, wuv in sorted(W[user_id].items(), key=operator.itemgetter(1), reverse=True)[0:K]:
 		# 将相似用户v中观看过的电影推荐给该用户
         for item_i, ri in user_item[v].items():
@@ -145,22 +170,51 @@ def Recommendation(user_item, user_id, W, K):
             # 电影推荐度 = 用户相似度 * 用户对电影兴趣度（或者评分）
             # 此例中用户设置观看过电影的兴趣度为1
             rank[user_id][item_i] += wuv * ri
-			
-    # 将推荐列表按分值从大到小排序
+
     rank_sorted = {}
-    rank_sorted[user_id] = sorted(rank[user_id].items(), key=operator.itemgetter(1), reverse=True)
+    rank_sorted[user_id] = sorted(rank[user_id].items(), key=operator.itemgetter(1), reverse=True)[0:R]
 
     return rank_sorted
 
+def PrecisionRecall(test, recommend):
+    # 推荐列表命中数
+    hit = 0
+    # 召回率分母（测试集中用户观看电影数）
+    n_recall = 0
+    # 准确率分母（推荐列表中电影数）
+    n_precision = 0
 
+    user_num = 0
+    for user, items in test.items():
+        # 推荐电影列表
+        reco_item = [item for item, score in recommend[user]]
+        # 推荐电影命中列表
+        hit_list = [item for item in reco_item if item in test[user]]
+
+        user_num += 1
+        hit += len(hit_list)
+        n_recall += len(test[user])
+        n_precision += len(reco_item)
+    print('user_num: {}, hit: {}, n_recall: {}, n_precision: {} '.format(user_num, hit, n_recall, n_precision))
+	
+    return [hit / (1.0 * n_recall), hit / (1.0 * n_precision)]
 
 if __name__ == '__main__':
 
     # 读取数据
-    df_data = pd.read_csv('/home/zwj/Desktop/recommend/uus_CF_demo.txt', header=None, names=['Movie', 'User', 'Rating'])
+    df_data = pd.read_csv('/home/zwj/Desktop/RSAlgorithms-master/data/ft_ratings.txt', \
+                          sep=' ', header=None, names=['User', 'Movie', 'Rating'])
+
+    print(df_data.sample(5))
+
+    # sample num: 35497
     data = df_data.values
+
     # 拆分训练集和测试集
-    train, test = SplitData(data, 2, 1, 1)
+    train, test = SplitData(data, 6, 1, 1)
+ 
+    print(len(train), len(test))
+
     # 生成user-tiem排列表
     user_item = UserItemDict(train)
     # 生成item-user排列表
@@ -171,9 +225,15 @@ if __name__ == '__main__':
     test_reco_list = {}
 
     # 遍历test数据集
-    for test_user in test:
+    for test_user in set(np.array(test)[:,0]):
 		# 生成单用户推荐列表
-		rank_list = Recommendation(user_item, test_user[0], user_sim, 3)
+		rank_list = Recommendation(user_item, int(test_user), user_sim, 20, 5)
 		# 合并到总的推荐字典中
 		test_reco_list.update(rank_list)
     print test_reco_list
+
+
+    test_user_item = TestUserItemDict(test)
+    recall, precision = PrecisionRecall(test_user_item, test_reco_list)
+
+    print(recall, precision)
